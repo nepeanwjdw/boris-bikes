458f0cb657d18ecb0008098c62481627022c2e55
alittlecross <alittlecross@live.co.uk>
2018-10-30 16:28:34 +0000

    added working? method and tests

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,14 @@
 require "dockingstation"
 describe DockingStation do
-it { is_expected.to respond_to(:release_bike) }
+  it { is_expected.to respond_to(:release_bike) }
+
+  it "gets bike" do 
+    station = DockingStation.new
+    expect(station.release_bike).not_to eq nil
+  end
+  it "is it working?" do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike.working?).to eq true
+  end
 end
