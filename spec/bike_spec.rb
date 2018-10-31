b0fd81c36b5c3ab75017bdc0a4de93484db92353
alittlecross <alittlecross@live.co.uk>
2018-10-31 16:21:59 +0000

    completed step 11

--- spec/bike_spec.rb
+++
@@ -1,4 +0,0 @@
-require "dockingstation"
-describe Bike do
-  it { is_expected.to respond_to(:working?) }
-end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,14 +1,21 @@
 require "dockingstation"
+
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
-  it "gets bike" do 
-    station = DockingStation.new
-    expect(station.release_bike).not_to eq nil
+  it "allows a bike to be docked" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
   end
+  it "get the bike from the docking station" do 
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
+  end
+end
+  
+describe Bike do
   it "is it working?" do
-    station = DockingStation.new
-    bike = station.release_bike
-    expect(bike.working?).to eq true
+    expect(subject.working?).to eq true
   end
 end
