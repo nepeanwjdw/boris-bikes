2550b443660e10116a34bd744e7faa73007480f8
will mcbrien <williamjamesmcbrien@gmail.com>
2018-11-01 14:35:04 +0000

    Refactor docking class to reflect SPR

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -6,13 +6,22 @@ class DockingStation
   end
 
   def release_bike
-    fail "No bikes available" if (@bikes.empty?)
+    fail "No bikes available" if empty?
     @bikes.first
   end
 
   def dock(bike)
-    fail "There's already 20 bikes here" if (@bikes.count == 20)
+    fail "There's already 20 bikes here" if full?
     @bikes << bike
+  end
+
+  private
+  def full?
+    @bikes.count == 20
+  end
+
+  def empty?
+   @bikes.empty?
   end
 end
 
